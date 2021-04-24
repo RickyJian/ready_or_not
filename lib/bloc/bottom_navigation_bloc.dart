cf8561963b4dc6cc94522062f22084db9acf4f6b
RickyJian <rjricky03bc@gmail.com>
2021-04-24 22:12:19 +0800

    add bottom navigation bloc

---
+++ lib/bloc/bottom_navigation_bloc.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+
+part 'bottom_navigation_event.dart';
+
+part 'bottom_navigation_state.dart';
+
+class BottomNavigationBloc
+    extends Bloc<BottomNavigationEvent, BottomNavigationState> {
+  int currentIndex = 0;
+
+  BottomNavigationBloc() : super(PageLoading());
+
+  @override
+  Stream<BottomNavigationState> mapEventToState(
+    BottomNavigationEvent event,
+  ) async* {
+    if (state is AppStarted) {
+      this.add(PageTapped(index: currentIndex));
+    } else if (event is PageTapped) {
+      this.currentIndex = event.index;
+      yield PageLoading();
+
+      switch (this.currentIndex) {
+        case 0:
+          {
+            yield HomePageLoaded();
+          }
+          break;
+        case 1:
+          {
+            yield AccountingPageLoaded();
+          }
+          break;
+        case 2:
+          {
+            yield TransactionPageLoaded();
+          }
+          break;
+        case 3:
+          {
+            yield SettingPageLoaded();
+          }
+          break;
+        default:
+          // TODO: add error page
+          break;
+      }
+    }
+  }
+}
