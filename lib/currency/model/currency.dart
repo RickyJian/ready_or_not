f6dfe2cb59f8a20183f4889165dd71b3a36c1f31
RickyJian <rjricky03bc@gmail.com>
2021-06-06 21:41:44 +0800

    update currency model

--- lib/currency/model/currency.dart
+++ lib/currency/model/currency.dart
@@ -1,16 +1,16 @@
 import 'package:decimal/decimal.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+
+enum CurrencyType { basic, special }
 
 class Currency extends Equatable {
   final String name;
+  final CurrencyType type;
   final Decimal exchangeRate;
 
-  Currency(
-    this.name, [
-    this.exchangeRate,
-  ]);
+  const Currency({@required this.name, @required this.type, this.exchangeRate});
 
   @override
-  List<Object> get props => [name, exchangeRate];
+  List<Object> get props => [name, type, exchangeRate];
 }
