f26c4b3576ecb9799c01da9e6688cd46fec072c2
RickyJian <rjricky03bc@gmail.com>
2021-06-06 22:51:15 +0800

    add create currencies and accounts table

--- lib/database.dart
+++ lib/database.dart
@@ -3,10 +3,33 @@ import 'package:sqflite/sqflite.dart';
 
 class DatabaseProvider {
   static const _version = 1;
+  static const _schema = 'ready_or_not.db';
   static final DatabaseProvider db = DatabaseProvider._();
   Database _database;
 
   DatabaseProvider._();
+
+  static const _createCurrencySql = '''
+  CREATE TABLE IF NOT EXISTS Currencies (
+    name TEXT PRIMARY KEY,
+    type INTEGER NOT NULL,
+    exchange_rate REAL NULL,
+  ) [WITHOUT ROWID];
+  ''';
+
+  static const _createAccountSql = '''
+    CREATE TABLE IF NOT EXISTS Accounts (
+    name TEXT PRIMARY KEY,
+    amount REAL NOT NULL,
+    enabled INTEGER NOT NULL,
+    Currencies_name Text NOT NULL,
+    memo TEXT NULL,
+    FOREIGN KEY (Currencies_name)
+    REFERENCES Currencies(name)
+      ON UPDATE NO ACTION 
+      ON DELETE NO ACTION;
+  ) [WITHOUT ROWID];
+  ''';
 
   Future<Database> get open async {
     if (_database != null) {
@@ -18,11 +41,12 @@ class DatabaseProvider {
 
   Future<Database> init() async {
     var path = await getDatabasesPath();
-    var fullPath = join(path, 'ready_or_not.db');
+    var fullPath = join(path, _schema);
     return await openDatabase(fullPath, version: _version, onCreate: _onCreate);
   }
 
   void _onCreate(Database db, int version) async {
-    // TODO:create table
+    await db.execute(_createCurrencySql);
+    await db.execute(_createAccountSql);
   }
 }
