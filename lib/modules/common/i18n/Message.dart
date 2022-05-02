eaa7bf27812038a526bf3411428ae928c242186f
RickyJian <rjricky03bc@gmail.com>
2022-05-02 23:15:07 +0800

    add account form i18n

--- lib/modules/common/i18n/Message.dart
+++ lib/modules/common/i18n/Message.dart
@@ -14,15 +14,45 @@ class Message extends Translations {
 
   // account
   // form
-  static const String accountName = "ACCOUNT_NAME";
+  static const String accountFormName = "ACCOUNT_FORM_NAME";
+  static const String accountFormPrice = "ACCOUNT_FORM_PRICE";
+  static const String accountFormType = "ACCOUNT_FORM_TYPE";
+  static const String accountFormUnit = "ACCOUNT_FORM_UNIT";
+  static const String accountFormEnabled = "ACCOUNT_FORM_ENABLED";
+  static const String accountFormMemo = "ACCOUNT_FORM_MEMO";
+  static const String accountFormButtonGroup = "ACCOUNT_FORM_BUTTON_GROUP";
+  static const String accountFormUnitNormal = "ACCOUNT_FORM_UNIT_NORMAL";
+  static const String accountFormUnitSpecial = "ACCOUNT_FORM_UNIT_SPECIAL";
+  static const String accountFormButtonClose = "ACCOUNT_FORM_BUTTON_CLOSE";
+  static const String accountFormButtonSave = "ACCOUNT_FORM_BUTTON_SAVE";
 
   @override
   Map<String, Map<String, String>> get keys => {
         'en_US': {
-          accountName: 'Name:',
+          accountFormName: 'Name:',
+          accountFormPrice: 'Price:',
+          accountFormType: 'Type:',
+          accountFormUnit: 'Unit:',
+          accountFormEnabled: 'Enabled:',
+          accountFormMemo: 'Memo:',
+          accountFormButtonGroup: 'Button Group',
+          accountFormUnitNormal: 'Normal',
+          accountFormUnitSpecial: 'Special',
+          accountFormButtonClose: 'Close',
+          accountFormButtonSave: 'Save',
         },
         'zh_TW': {
-          accountName: '名稱：',
+          accountFormName: '名稱：',
+          accountFormPrice: '金額：',
+          accountFormType: '類型：',
+          accountFormUnit: '單位：',
+          accountFormEnabled: '啟用：',
+          accountFormMemo: '備註：',
+          accountFormButtonGroup: '按鈕群組',
+          accountFormUnitNormal: '一般',
+          accountFormUnitSpecial: '特殊',
+          accountFormButtonClose: '關閉',
+          accountFormButtonSave: '存檔',
         }
       };
 }
