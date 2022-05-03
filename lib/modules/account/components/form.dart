56b87a46d7586a342e5df51fd63862d1a4505167
RickyJian <rjricky03bc@gmail.com>
2022-05-04 00:13:11 +0800

    binding onChange func

--- lib/modules/account/components/form.dart
+++ lib/modules/account/components/form.dart
@@ -26,26 +26,29 @@ class AccountFormText extends AccountForm {
 class AccountFormDropdown extends AccountForm {
   @override
   final String label;
+  final int selectedIndex;
   final List<AccountFormDropdownItem> items;
 
-  const AccountFormDropdown({required this.label, required this.items}) : super(label: label);
+  final ValueChanged onChanged;
 
-  // TODO: onChanged callback
+  const AccountFormDropdown({required this.label, required this.items, required this.onChanged, this.selectedIndex = 0})
+      : super(label: label);
 
   @override
   List<Object?> get props => [label, items];
 }
 
 class AccountFormDropdownItem extends AccountForm {
+  final int index;
   final String label;
 
-  const AccountFormDropdownItem({required this.label}) : super(label: label);
+  const AccountFormDropdownItem({required this.index, required this.label}) : super(label: label);
 
   @override
   String toString() => label;
 
   @override
-  List<Object?> get props => [label];
+  List<Object?> get props => [index, label];
 }
 
 class AccountFormSwitchButton extends AccountForm {
